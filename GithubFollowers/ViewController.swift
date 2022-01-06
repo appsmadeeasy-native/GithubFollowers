750942619d55d02a4f096deaa7d14b4a353da7f0
Syed Mahmud <sjmahmud@gmail.com>
2022-01-06 13:43:40 -0800

    Initial Commit

---
+++ GithubFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubFollowers
+//
+//  Created by Syed Mahmud on 1/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
